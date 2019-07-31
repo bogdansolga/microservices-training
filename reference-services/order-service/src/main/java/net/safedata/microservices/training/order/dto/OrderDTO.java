18c7d93aec0436634d0d6114bfdde791be7b2911
Bogdan Solga <bogdan.solga@gmail.com>
2019-07-31 13:52:29 +0300

    [dev] Added the name for the Order, added a simple logging statement

--- reference-services/order-service/src/main/java/net/safedata/microservices/training/order/dto/OrderDTO.java
+++ reference-services/order-service/src/main/java/net/safedata/microservices/training/order/dto/OrderDTO.java
@@ -3,4 +3,14 @@ package net.safedata.microservices.training.order.dto;
 import java.io.Serializable;
 
 public class OrderDTO implements Serializable {
+
+    private final String productName;
+
+    public OrderDTO(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
 }
--- reference-services/order-service/src/main/java/net/safedata/microservices/training/order/service/OrderService.java
+++ reference-services/order-service/src/main/java/net/safedata/microservices/training/order/service/OrderService.java
@@ -4,12 +4,16 @@ import net.safedata.microservices.training.order.dto.OrderDTO;
 import net.safedata.microservices.training.order.events.OrderCreatedEvent;
 import net.safedata.microservices.training.order.marker.InboundPort;
 import net.safedata.microservices.training.order.ports.MessagingOutboundPort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderService implements InboundPort {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderService.class);
 
     private final MessagingOutboundPort messagingOutboundPort;
 
@@ -20,7 +24,10 @@ public class OrderService implements InboundPort {
 
     @Transactional
     public void createOrder(OrderDTO orderDTO) {
-        // insert magic here
+        LOGGER.info("Creating an order for the product {}...", orderDTO.getProductName());
+
+        // TODO insert magic here
+
         messagingOutboundPort.publishEvent(new OrderCreatedEvent(230));
     }
 }

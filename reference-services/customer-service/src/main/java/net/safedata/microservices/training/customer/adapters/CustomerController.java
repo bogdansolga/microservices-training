7ddf7edd10482e870939e11968fb8d5866f06208
Bogdan Solga <bogdan.solga@gmail.com>
2019-07-31 14:31:57 +0300

    [fix] Fixed the main URL

--- reference-services/customer-service/src/main/java/net/safedata/microservices/training/customer/adapters/CustomerController.java
+++ reference-services/customer-service/src/main/java/net/safedata/microservices/training/customer/adapters/CustomerController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/customer")
 public class CustomerController implements InboundAdapter {
 
     private final CustomerService customerService;

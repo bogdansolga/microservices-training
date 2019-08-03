75f6477a7832a8ac57dce9e27540c41aa59929b5
Bogdan Solga <bogdan.solga@gmail.com>
2019-08-03 11:30:22 +0300

    [improve] Restrict the bounds of the created messages

--- reference-services/customer-service/src/main/java/net/safedata/microservices/training/customer/adapters/MessageCreator.java
+++ reference-services/customer-service/src/main/java/net/safedata/microservices/training/customer/adapters/MessageCreator.java
@@ -1,15 +1,14 @@
 package net.safedata.microservices.training.customer.adapters;
 
+import net.safedata.microservices.training.customer.messages.AbstractMessage;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.MimeTypeUtils;
 
-import java.io.Serializable;
-
 final class MessageCreator {
 
-    static  <T extends Serializable> Message<?> create(final T t) {
+    static  <T extends AbstractMessage> Message<?> create(final T t) {
         return MessageBuilder.withPayload(t)
                              .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
                              .build();

ba2598827a1453391fc750111f3d188fd7e52fcc
Bogdan Solga <bogdan.solga@gmail.com>
2021-04-12 09:53:47 +0300

    [fix] Fixed the tests imports

--- reference-services/shipping-service/src/test/java/net/safedata/microservices/training/shipping/ShippingServiceApplicationTests.java
+++ reference-services/shipping-service/src/test/java/net/safedata/microservices/training/shipping/ShippingServiceApplicationTests.java
@@ -1,16 +1,13 @@
 package net.safedata.microservices.training.shipping;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShippingServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }

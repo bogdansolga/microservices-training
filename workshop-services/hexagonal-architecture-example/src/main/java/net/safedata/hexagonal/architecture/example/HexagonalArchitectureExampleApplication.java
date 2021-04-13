02540a6d1926fd258a9d8b718d5e7b93d7a602d8
Bogdan Solga <bogdan.solga@gmail.com>
2021-04-13 12:34:35 +0300

    [dev] Added the initial infrastructure for the hexagonal architecture example

---
+++ workshop-services/hexagonal-architecture-example/src/main/java/net/safedata/hexagonal/architecture/example/HexagonalArchitectureExampleApplication.java
@@ -0,0 +1,12 @@
+package net.safedata.hexagonal.architecture.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureExampleApplication.class, args);
+    }
+}

b29cb45d48f70a36332b6479e32b1828a150d3ef
Bogdan Solga <bogdan.solga@gmail.com>
2021-06-11 10:30:34 +0300

    [dev] Added the structure for the Orders and Shipping services

---
+++ workshop-day/order-service/src/main/java/com/microservices/workshop/orders/OrdersServiceMain.java
@@ -0,0 +1,12 @@
+package com.microservices.workshop.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersServiceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersServiceMain.class, args);
+    }
+}
---
+++ workshop-day/shipping-service/src/main/java/com/microservices/workshop/shipping/ShippingServiceMain.java
@@ -0,0 +1,12 @@
+package com.microservices.workshop.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingServiceMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShippingServiceMain.class, args);
+    }
+}

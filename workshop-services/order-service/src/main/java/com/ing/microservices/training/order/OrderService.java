58c5a3425c259b333f265579619684b5110a03e0
Bogdan Solga <bogdan.solga@gmail.com>
2023-09-05 11:52:28 +0300

    [dev] Initial workshop services

---
+++ workshop-services/order-service/src/main/java/com/ing/microservices/training/order/OrderService.java
@@ -0,0 +1,12 @@
+package com.ing.microservices.training.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderService.class, args);
+    }
+}

dacdfceabdb8323fcb5e5873166e1929287f285a
Bogdan Solga <bogdan.solga@gmail.com>
2023-10-20 11:08:42 +0300

    [dev] Added a minimal billing service

---
+++ workshop-services/workshop-billing-service/src/main/java/net/safedata/microservices/training/billing/WorkshopBillingService.java
@@ -0,0 +1,12 @@
+package net.safedata.microservices.training.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopBillingService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopBillingService.class, args);
+    }
+}
